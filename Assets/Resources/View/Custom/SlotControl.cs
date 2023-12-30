8e26ebddeec8493843e834217dcfbf01ca3796bf
jvh <Joni1234@hotmail.dk>
2023-12-30 15:54:27 +0100

    Moved ui elements and made the custom ui work

---
+++ Assets/Resources/View/Custom/SlotControl.cs
@@ -0,0 +1,49 @@
+using UnityEngine.UIElements;
+
+namespace YAPCG.Resources.View.Custom
+{
+    [UxmlElement]
+    public partial class SlotControl : VisualElement
+    {
+        
+        private string _label, _value;
+        private readonly Label _nameLabel, _valueLabel;
+        
+        [UxmlAttribute]
+        public string Label
+        {
+            get => _label;
+            private set
+            {
+                if (_label == value)
+                    return;
+                _label = value;
+                _nameLabel.name = value;
+            }
+        }
+        
+        [UxmlAttribute]
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value)
+                    return;
+                _value = value;
+                _valueLabel.text = value;
+            }
+        }
+
+
+        public SlotControl()
+        {
+            VisualElement body = UnityEngine.Resources.Load<VisualTreeAsset>($"View/Custom/Slots").CloneTree();
+            Add(body);
+            _nameLabel = body.Q<Label>("label");
+            _valueLabel = body.Q<Label>("value");
+            _valueLabel.text = _value;
+            _nameLabel.text = _label;
+        }
+    }
+}
