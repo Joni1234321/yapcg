0520464fa02d185746db3e299e25303f02384cae
jvh <Joni1234@hotmail.dk>
2023-12-30 16:41:22 +0100

    Added coloring of slots

--- Assets/Resources/View/Custom/SlotControl.cs
+++ Assets/Resources/View/Custom/SlotControl.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace YAPCG.Resources.View.Custom
@@ -7,18 +10,21 @@ namespace YAPCG.Resources.View.Custom
     {
         
         private string _label, _value;
-        private readonly Label _nameLabel, _valueLabel;
+        private Color _hue;
+        
+        private readonly Label _labelLabel, _valueLabel;
+        private readonly List<VisualElement> _splits; 
         
         [UxmlAttribute]
         public string Label
         {
             get => _label;
-            private set
+            set
             {
                 if (_label == value)
                     return;
                 _label = value;
-                _nameLabel.name = value;
+                _labelLabel.text = value;
             }
         }
         
@@ -35,15 +41,40 @@ namespace YAPCG.Resources.View.Custom
             }
         }
 
+        [UxmlAttribute]
+        public Color Hue
+        {
+            get => _hue;
+            set
+            {
+                if (_hue == value)
+                    return;
+
+                _hue = value;
+                
+                Color.RGBToHSV(value, out float hue, out float sat, out float val);
+                const float LOW_SV = 0.34f, HIGH_SV = 0.7f;
+                for (int i = 0; i < _splits.Count; i++)
+                {
+                    float t = (float)i / _splits.Count;
+                    float sv = math.lerp(LOW_SV, HIGH_SV, t);
+                    _splits[i].style.backgroundColor = Color.HSVToRGB(hue, sv, sv - .1f);
+                }
+            }
+        }
 
         public SlotControl()
         {
             VisualElement body = UnityEngine.Resources.Load<VisualTreeAsset>($"View/Custom/Slots").CloneTree();
             Add(body);
-            _nameLabel = body.Q<Label>("label");
+            _labelLabel = body.Q<Label>("label");
             _valueLabel = body.Q<Label>("value");
+            _splits = body.Query<VisualElement>("split").ToList();
+            
+            
             Label = "S";
             Value = 12.ToString();
+            Hue = Color.HSVToRGB(0, 1, 1);
         }
     }
 }
