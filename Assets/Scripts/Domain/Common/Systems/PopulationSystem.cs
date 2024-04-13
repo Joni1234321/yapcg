e6c285da4c4c9c059efef61b05bb8ac3217ea622
Joni <Joni1234@hotmail.dk>
2024-04-13 16:54:54 +0200

    population

---
+++ Assets/Scripts/Domain/Common/Systems/PopulationSystem.cs
@@ -0,0 +1,58 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using YAPCG.Domain.Common.Components;
+using YAPCG.Domain.NUTS;
+using YAPCG.Engine.Common;
+using YAPCG.Engine.Components;
+using YAPCG.Engine.Time.Systems;
+
+namespace YAPCG.Domain.Common.Systems
+{
+    [UpdateInGroup(typeof(TickWeeklyGroup))]
+    [UpdateAfter(typeof(ProductionSystem))]
+    public partial struct PopulationSystem : ISystem
+    {
+        private const float MIN_GROWTH = 0.01f, MAX_GROWTH = 0.02f;
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<SharedRandom>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            Random random = SystemAPI.GetSingleton<SharedRandom>().Random;
+            new MigrationJob().Run();
+            new PopulationGrowthJob { Random = random, MinGrowth = MIN_GROWTH, MaxGrowth = MAX_GROWTH }.Run();
+        }
+    }
+    
+    [BurstCompile]
+    partial struct MigrationJob : IJobEntity
+    {
+        void Execute(in Deposit.Reserves reserves, ref Labor labor, ref LaborExtras extras)
+        {
+            extras.MigrationValue = (int)(reserves.Value / labor.Population);
+            extras.EmigrationValue = (int)(reserves.Value / labor.Population);
+        }
+    }
+
+    [BurstCompile]
+    partial struct PopulationGrowthJob : IJobEntity
+    {
+        public Random Random;
+        public float MinGrowth, MaxGrowth;
+
+        void Execute(ref Labor labor, ref LaborExtras extras)
+        {
+            float growth = Random.NextFloat(MinGrowth, MaxGrowth);
+            float modifier = mathutils.tanh_diff(labor.Population / labor.Ceiling);
+            
+            extras.LaborDifference = (long)math.ceil(labor.Population * growth * modifier);
+            labor.Population += extras.LaborDifference;
+        }
+    }
+}
---
+++ Assets/Scripts/Engine/Common/mathutils.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Mathematics;
+
+namespace YAPCG.Engine.Common
+{
+    public struct mathutils
+    {
+        private const float STEEPNESS = 2;
+
+        
+        /// <summary>
+        /// result is ]-1, 1[
+        /// </summary>
+        public static float hyberbolic(float x) => math.tanh(x);
+
+        /// <summary>
+        /// dx/dt tanh
+        /// </summary>
+        public static float sech2(float x) => 1 / math.pow(math.cosh(x), 2);
+        
+        
+        public static float logistic(float x) => 1 / (1 + math.exp(-x));
+
+        /// <summary>
+        /// x and result is between [0, 1] 
+        /// </summary>
+        public static float tanh_norm(float x) => 0.5f * (math.tanh((x - .5f) * STEEPNESS) + 1);
+
+        /// <summary>
+        /// result ]0,1[
+        /// </summary>
+        public static float tanh_diff(float x) => sech2(x * STEEPNESS);
+    }
+}
