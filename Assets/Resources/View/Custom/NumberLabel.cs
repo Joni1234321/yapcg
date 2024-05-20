7702d82e13f26fd9c108fb34b77317b4b65a6efd
jvh <Joni1234@hotmail.dk>
2024-05-20 09:45:19 +0200

    Added number label

---
+++ Assets/Resources/View/Custom/NumberLabel.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using UnityEngine.UIElements;
+
+namespace YAPCG.Resources.View.Custom
+{
+    [UxmlElement]
+    public partial class NumberLabel : CustomUI
+    {
+
+        private readonly Label _titleLabel, _valueLabel;
+
+        private string _title, _value;
+
+        [UxmlAttribute]
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title == value)
+                    return;
+                _title = value;
+                _titleLabel.text = value;
+            }
+        }
+
+        [UxmlAttribute]
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value)
+                    return;
+                _value = value;
+                UpdateProgressBarTitle(value, _max);
+            }
+        }
+
+      
+        public ProgressBarControl() : base("View/Custom/numberlabel")
+        {
+            _changeLabel = Q<Label>("change");
+            _titleLabel = Q<Label>("title");
+            _progressBar = Q<ProgressBar>();
+
+            Title = "Progress";
+            Max = 100;
+            Value = 20;
+            Change = 4;
+        }
+    }
+}
