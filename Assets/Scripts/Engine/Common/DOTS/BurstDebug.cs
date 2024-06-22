e11fc8b9f0d43d405772d6d0d5b96f1e0058fb63
Joni <Joni1234@hotmail.dk>
2024-06-22 16:02:17 +0200

    Added package

---
+++ Assets/Scripts/Engine/Common/DOTS/BurstDebug.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Burst;
+
+namespace YAPCG.Engine.Common.DOTS
+{
+    public class BurstDebug
+    {
+        [BurstDiscard]
+        private static void SetIfManaged(ref bool b) => b = false;
+
+        public static bool IsBurst()
+        {
+            var b = true;
+            SetIfManaged(ref b);
+            return b;
+        }
+    }
+}
